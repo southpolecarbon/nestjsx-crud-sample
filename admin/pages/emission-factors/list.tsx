228b2e8ad00a0bd54a26937ef46a608395f78e22
Chathula <schathula@gmail.com>
2023-01-24 00:21:32 +0100

    created the cli to insert the data

--- admin/pages/emission-factors/list.tsx
+++ admin/pages/emission-factors/list.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { IResourceComponentsProps, BaseRecord } from '@pankod/refine-core';
+import {
+  IResourceComponentsProps,
+  BaseRecord,
+  CrudFilters,
+  HttpError,
+} from '@pankod/refine-core';
 import {
   useTable,
   List,
@@ -8,34 +13,91 @@ import {
   EditButton,
   ShowButton,
   DeleteButton,
+  Row,
+  Col,
+  Form,
+  Input,
+  Button,
+  Icons,
 } from '@pankod/refine-antd';
 
+interface IEmissionFactor {
+  name: string;
+  activity_id: string;
+}
+
 export const EmissionFactorsList: React.FC<IResourceComponentsProps> = () => {
-  const { tableProps } = useTable({
+  const { tableProps, searchFormProps } = useTable<
+    IEmissionFactor,
+    HttpError,
+    { q: string }
+  >({
     syncWithLocation: true,
+    onSearch: (params) => {
+      const filters: CrudFilters = [];
+      const { q } = params;
+
+      filters.push(
+        {
+          field: 'name',
+          operator: 'contains',
+          value: q,
+        },
+        {
+          field: 'activity_id',
+          operator: 'contains',
+          value: q,
+        }
+      );
+
+      return filters;
+    },
   });
 
   return (
-    <List>
-      <Table {...tableProps} rowKey="id">
-        <Table.Column dataIndex="name" title="NAME" />
-        <Table.Column dataIndex="source" title="SOURCE" />
-        <Table.Column dataIndex="year" title="YEAR" />
-        <Table.Column dataIndex="region" title="REGION" />
-        <Table.Column dataIndex="sector" title="SECTOR" />
-        <Table.Column dataIndex="category" title="CATEGORY" />
-        <Table.Column
-          title="Actions"
-          dataIndex="actions"
-          render={(_, record: BaseRecord) => (
-            <Space>
-              <EditButton hideText size="small" recordItemId={record.id} />
-              <ShowButton hideText size="small" recordItemId={record.id} />
-              <DeleteButton hideText size="small" recordItemId={record.id} />
-            </Space>
-          )}
-        />
-      </Table>
-    </List>
+    <Row gutter={[16, 16]}>
+      <Col lg={6} xs={24}>
+        <Form layout="vertical" {...searchFormProps}>
+          <Form.Item label="Search" name="q">
+            <Input
+              placeholder="name, activity_id"
+              prefix={<Icons.SearchOutlined />}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              Filter
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+      <Col lg={18} xs={24}>
+        <List>
+          <Table {...tableProps} rowKey="id">
+            <Table.Column dataIndex="name" title="NAME" />
+            <Table.Column dataIndex="source" title="SOURCE" />
+            <Table.Column dataIndex="year" title="YEAR" />
+            <Table.Column dataIndex="region" title="REGION" />
+            <Table.Column dataIndex="sector" title="SECTOR" />
+            <Table.Column dataIndex="category" title="CATEGORY" />
+            <Table.Column
+              title="Actions"
+              dataIndex="actions"
+              render={(_, record: BaseRecord) => (
+                <Space>
+                  <EditButton hideText size="small" recordItemId={record.id} />
+                  <ShowButton hideText size="small" recordItemId={record.id} />
+                  <DeleteButton
+                    hideText
+                    size="small"
+                    recordItemId={record.id}
+                  />
+                </Space>
+              )}
+            />
+          </Table>
+        </List>
+      </Col>
+    </Row>
   );
 };
